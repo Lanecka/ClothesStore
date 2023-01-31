d8c79d1ce2861b20be0d6b92e0ec3b3407f91b94
Murreya <ladelaym@gmail.com>
2023-01-31 13:38:39 +0300

    верстка и подключение карточек

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import DrawerPage from './pages/DrawerPage/DrawerPage';
+import MainPage from './pages/MainPage/MainPage';
+
+function App() {
+  return (
+    <div className="wrapper">
+      <DrawerPage />
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
